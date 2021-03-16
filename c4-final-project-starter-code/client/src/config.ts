c1764885d121f969158bb2e7f7aa772885edf863
Ubuntu <ubuntu@ip-172-31-0-29.eu-central-1.compute.internal>
2021-03-16 09:08:02 +0000

    client config changed

--- c4-final-project-starter-code/client/src/config.ts
+++ c4-final-project-starter-code/client/src/config.ts
@@ -6,5 +6,5 @@ export const authConfig = {
   
   domain: 'dev-o5x2h8gz.eu.auth0.com',            // Auth0 domain
   clientId: '0GGMSjgS8MBEvLAEvVSgIpdyyydJ4my1',          // Auth0 client id
-  callbackUrl: 'http://localhost:8080/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
